7f019810a6d9e7552947abd5d70f003b9aab0d84
tresoneur <tresoneur@gmail.com>
2021-02-20 13:08:16 +0100

    Switch to .NET 6 Preview 1

--- Startup.cs
+++ Startup.cs
@@ -15,8 +15,6 @@ namespace Caerostris.Server
 
         public IConfiguration Configuration { get; }
 
-        // This method gets called by the runtime. Use this method to add services to the container.
-        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
 
@@ -24,7 +22,6 @@ namespace Caerostris.Server
             services.AddRazorPages();
         }
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             if (env.IsDevelopment())
@@ -35,7 +32,6 @@ namespace Caerostris.Server
             else
             {
                 app.UseExceptionHandler("/Error");
-                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
 
